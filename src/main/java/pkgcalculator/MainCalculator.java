738eebec254a876556548e2ab7c43bb1f20891c5
PRINCE1KR <prince.kumar1@nexturn.com>
2023-03-11 23:41:13 +0530

    third

--- src/main/java/pkgcalculator/MainCalculator.java
+++ src/main/java/pkgcalculator/MainCalculator.java
@@ -13,9 +13,9 @@ public class MainCalculator {
 		}else {
 			System.out.println("Failed");
 		}
-		int resultAddition=objscientificCalculator.addition(12, 12);
+		int resultAddition=objscientificCalculator.addition(12, 13);
 		
-		if(resultAddition==24){
+		if(resultAddition==25){
 			System.out.println("Passed");
 		}else {
 			System.out.println("Failed");
